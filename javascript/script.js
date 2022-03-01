73d7167de90542c172ee98e3b51b64e65b494952
DaleteB <daletepokry@hotmail.com>
2022-03-01 18:00:35 +0000

    Added all the information in HTML

---
+++ javascript/script.js
@@ -0,0 +1,69 @@
+let newDate = new Date();
+
+let h4 = document.querySelector("h4");
+
+let hours = newDate.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = newDate.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[newDate.getDay()];
+
+h4.innerHTML = `${day} ${hours}:${minutes}`;
+
+function searchForm(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city");
+  console.log(cityInput.value);
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${cityInput.value}`;
+
+  let apiKey = "8470682d02ce3ef83d09047bbc48b960";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(searchCity);
+}
+let buttonSubmit = document.querySelector("#submitCity");
+buttonSubmit.addEventListener("click", searchForm);
+
+function searchCity(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temp = document.querySelector("#currentTemperature");
+  temp.innerHTML = `${temperature}°C`;
+}
+
+function handlePosition(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "8470682d02ce3ef83d09047bbc48b960";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(iAmHere);
+}
+
+function iAmHere(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let location = response.data.name;
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${location}`;
+  let temp = document.querySelector("#currentTemperature");
+  temp.innerHTML = `${temperature}°C`;
+}
+let buttonRequest = document.querySelector("#requestLocation");
+buttonRequest.addEventListener("click", getPosition);
+
+function getPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(handlePosition);
+}
